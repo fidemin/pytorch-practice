f0f1ba8e5955c812b2b7a8e8882d39bf3e59c587
Yunhong Min <yunhongmin00@gmail.com>
2024-12-31 10:58:28 +0900

    apply metrics constants

--- src/luna/app.py
+++ src/luna/app.py
@@ -13,6 +13,12 @@ from src.luna.model import LunaModel
 logger = logging.getLogger(__name__)
 
 
+METRICS_LABEL_IDX = 0
+METRICS_PREDICTION_IDX = 1
+METRICS_LOSS_IDX = 2
+METRICS_SIZE = 3
+
+
 class LunaTrainingApp(App):
     def __init__(self, *argv):
         if not argv:
@@ -47,7 +53,8 @@ class LunaTrainingApp(App):
         self.optimizer = self._init_optimizer()
 
         # train metrics per epoch
-        self._train_metric_list = []
+        self._train_metrics_list = []
+        self._valid_metrics_list = []
 
     def run(self):
         print(f"Running with args: {self.args}")
@@ -60,15 +67,31 @@ class LunaTrainingApp(App):
             validation_ratio=0.01,
         )
 
-        data_loader = DataLoader(
+        valid_ds = LunaDataset(
+            self.args.candidate_file_path,
+            self.args.annotation_file_path,
+            self.args.ct_files_dir,
+            is_validation=True,
+            validation_ratio=0.01,
+        )
+
+        train_dl = DataLoader(
             train_ds,
             batch_size=self.args.batch_size,
             num_workers=self.args.num_workers,
             pin_memory=self.use_cuda or self.use_mps,
         )
 
+        valid_dl = DataLoader(
+            valid_ds,
+            batch_size=self.args.batch_size,
+            num_workers=self.args.num_workers,
+            pin_memory=self.use_cuda or self.use_mps,
+        )
+
         for epoch in range(1, self.args.num_epochs + 1):
-            self._train_epoch(epoch, data_loader)
+            self._train_epoch(epoch, train_dl)
+            self._validate_epoch(epoch, valid_dl)
 
     def _get_device(self):
         if self.use_cuda:
@@ -99,7 +122,7 @@ class LunaTrainingApp(App):
 
         # save metrics for one epoch
         train_metrics = torch.zeros(
-            3,  # kind of metrics
+            METRICS_SIZE,  # kind of metrics
             len(data_loader.dataset),
             device=self.device,
         )
@@ -114,9 +137,33 @@ class LunaTrainingApp(App):
             loss_var.backward()
             self.optimizer.step()
 
-            logger.info(f"Epoch={epoch} Batch Index={batch_idx} Loss={loss_var.item()}")
+            logger.info(
+                f"Train: Epoch={epoch} Batch Index={batch_idx} Loss={loss_var.item()}"
+            )
 
-        self._train_metric_list.append(train_metrics.to("cpu"))
+        self._train_metrics_list.append(train_metrics.to("cpu"))
+
+    def _validate_epoch(self, epoch, data_loader):
+        self.model.eval()
+
+        # save metrics for one epoch
+        valid_metrics = torch.zeros(
+            METRICS_SIZE,  # kind of metrics
+            len(data_loader.dataset),
+            device=self.device,
+        )
+
+        with torch.no_grad():
+            for batch_idx, (data, target) in enumerate(data_loader):
+                loss_var = self._compute_batch_loss(
+                    batch_idx, data, target, valid_metrics
+                )
+
+                logger.info(
+                    f"Validation: Epoch={epoch} Batch Index={batch_idx} Loss={loss_var.item()}"
+                )
+
+        self._valid_metrics_list.append(valid_metrics.to("cpu"))
 
     def _compute_batch_loss(self, batch_idx, data, target, train_metrics):
         data = data.to(self.device, non_blocking=True)
@@ -142,6 +189,8 @@ class LunaTrainingApp(App):
         start_ndx = batch_idx * batch_size
         end_idx = start_ndx + batch_size
 
-        train_metrics[0, start_ndx:end_idx] = target[:, 1].detach()
-        train_metrics[1, start_ndx:end_idx] = probability[:, 1].detach()
-        train_metrics[2, start_ndx:end_idx] = loss_variable.detach()
+        train_metrics[METRICS_LABEL_IDX, start_ndx:end_idx] = target[:, 1].detach()
+        train_metrics[METRICS_PREDICTION_IDX, start_ndx:end_idx] = probability[
+            :, 1
+        ].detach()
+        train_metrics[METRICS_LOSS_IDX, start_ndx:end_idx] = loss_variable.detach()
